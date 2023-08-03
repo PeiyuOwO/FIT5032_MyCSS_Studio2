98f0daa21b52a41372f0d78ab2e5ce40882c5715
Peiyu Wu <peiyu0233@gmail.com>
2023-08-03 16:26:33 +1000

    Add project files.

---
+++ FIT5032_MyCSS_Studio2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FIT5032_MyCSS_Studio2.Startup))]
+namespace FIT5032_MyCSS_Studio2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
